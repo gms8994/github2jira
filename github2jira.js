75b91e31c00611c722e015d2f39b74702737a14f
Glendon Solsberry <gsolsberry@learninghouse.com>
2013-02-22 11:26:30 -0500

    Make sure to link all instances found in a single node; add additional nodes to search

--- github2jira.js
+++ github2jira.js
@@ -1,25 +1,29 @@
 var titles = document.querySelectorAll('.discussion-topic-title');
 var messages = document.querySelectorAll('.message');
+var comments = document.querySelectorAll('div.comment-content div div p');
 
 titles = Array.prototype.slice.call(titles);
 messages = Array.prototype.slice.call(messages);
+comments = Array.prototype.slice.call(comments);
 
-var nodes = titles.concat(messages);
+var nodes = comments.concat(titles.concat(messages));
 
 for (var i = 0; i < nodes.length; i++) {
 	var node = nodes[i];
 
 	var orig_content = content = node.textContent;
-	var found = content.match(/\b#?(\w*?-\d+)\b/);
-	console.log(found);
+	var found = content.match(/\b#?(\w*?-\d+)\b/g);
 
 	var match_text;
-	if (found && found[1]) {
-		match_text = found[1];
+	if (found) {
+		for (var counter = 0; counter < found.length; counter++) {
+			var element = found[counter];
+			match_text = element;
 
-		match_text = '<a href="https://learninghouse.atlassian.net/browse/' + match_text + '" target="_blank" style="color: orange;">' + match_text + '</a>';
-		var re = new RegExp(escapeRegExp(found[1]));
-		content = content.replace(re, match_text);
+			match_text = '<a href="https://learninghouse.atlassian.net/browse/' + match_text + '" target="_blank" style="color: orange;">' + match_text + '</a>';
+			var re = new RegExp(escapeRegExp(element));
+			content = content.replace(re, match_text);
+		}
 	}
 
 	if (orig_content !== content) {
